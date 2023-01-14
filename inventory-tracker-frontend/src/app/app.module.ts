1eeb677bcdbf26f2c8822f1b90ba039b4ad87794
Karina Lage <karinar_lage@live.com>
2023-01-14 15:15:58 -0600

    Added registration page component

--- inventory-tracker-frontend/src/app/app.module.ts
+++ inventory-tracker-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
